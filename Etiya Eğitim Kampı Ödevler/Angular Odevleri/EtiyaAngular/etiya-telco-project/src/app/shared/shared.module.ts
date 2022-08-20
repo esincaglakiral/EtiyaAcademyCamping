130dca7dc72912c8fb0e90f00e688073d033279d
Esin Çağla KIRAL <caglakiral@gmail.com>
2022-08-20 03:55:48 +0300

    Components added to shared module and app folder updated

--- Etiya Eğitim Kampı Ödevler/Angular Odevleri/EtiyaAngular/etiya-telco-project/src/app/shared/shared.module.ts
+++ Etiya Eğitim Kampı Ödevler/Angular Odevleri/EtiyaAngular/etiya-telco-project/src/app/shared/shared.module.ts
@@ -1,16 +1,50 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverlayTitleComponent } from './components/overlay-title/overlay-title.component';
+import { SideFilterComponent } from './components/side-filter/side-filter.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SettingsMenuComponent } from './components/settings-menu/settings-menu.component';
+import { TableInfoComponent } from './components/table-info/table-info.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { InfoTitleComponent } from './components/info-title/info-title.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ContainerComponent } from './components/container/container.component';
+import { ConfirmComponent } from './components/confirm/confirm.component';
+import { WarningMessageComponent } from './components/warning-message/warning-message.component';
 
 @NgModule({
   declarations: [
-    OverlayTitleComponent
+    OverlayTitleComponent,
+    SideFilterComponent,
+    SettingsMenuComponent,
+    NavbarComponent,
+    UserInfoComponent,
+    TableInfoComponent,
+    InfoTitleComponent,
+    MainLayoutComponent,
+    NotFoundComponent,
+    ContainerComponent,
+    WarningMessageComponent,
+    ConfirmComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    OverlayTitleComponent
+    OverlayTitleComponent,
+    SideFilterComponent,
+    SettingsMenuComponent,
+    NavbarComponent,
+    UserInfoComponent,
+    TableInfoComponent,
+    InfoTitleComponent,
+    MainLayoutComponent,
+    NotFoundComponent,
+    ContainerComponent,
+    WarningMessageComponent,
+    ConfirmComponent
+    
     
   ]
 })
