14e20e376e09e0f3cfe952f4de43aff7b0dbe513
Esin Çağla KIRAL <caglakiral@gmail.com>
2022-08-24 19:05:49 +0300

    add customer-dashboard and customer service

--- EtiyaEgitimKampiOdevler/AngularOdevleri/EtiyaAngular/etiya-telco-project/src/app/features/customers/pages/customer-dashboard/customer-dashboard.component.ts
+++ EtiyaEgitimKampiOdevler/AngularOdevleri/EtiyaAngular/etiya-telco-project/src/app/features/customers/pages/customer-dashboard/customer-dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Customer } from './../../models/customer';
-import { CustomersService } from './../../services/customers/customers.service';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CustomersService } from '../../services/customer/customers.service';
 
 @Component({
   templateUrl: './customer-dashboard.component.html',
@@ -8,38 +8,23 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CustomerDashboardComponent implements OnInit {
   customerList! : Customer[];
-  customerList1! : Customer[];
+  filteredCustomerList! : Customer[];
   lenght!:number;
-
-//@Input() filteredData! : Customer[];
 
   constructor(private customersService: CustomersService) { }
 
   ngOnInit(): void {
     this.getCustomersList();
-
-    //console.warn(this.filteredData)
   }
 
   getCustomersList(){
     this.customersService.getList().subscribe(response=>{
       this.customerList= response
-      // console.log(this.customerList)
-      // console.log(this.customerList[0].addresses[0].city.name)
-      // console.log(this.customerList[0])
-
     })
   }
 
-
   search(event:any){
-    console.log("bbbbb")
-
-    console.log(event)
-    this.customerList1 = event
-    this.lenght = this.customerList1.length
-
+    this.filteredCustomerList = event
+    this.lenght = this.filteredCustomerList.length
   }
-
-
 }
